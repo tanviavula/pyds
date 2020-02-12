0173ff9dfc6d28c669450a2c448534befcdcd877
unknown <lakshman.avula@outlook.com>
2020-02-12 10:57:39 +0530

    added final project

---
+++ final_project/serverapp/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+   
+    data = request.get_json()
+    total_sqft = float(data['total_sqft'])
+    location = data['location']
+    bhk = int(data['bhk'])
+    bath = int(data['bath'])
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+   
+    return response
+
+if __name__ == "__main__":
+    util.load_saved_artifacts()
+    app.run()
